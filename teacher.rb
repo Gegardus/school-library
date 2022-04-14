2db82af29184e4ff27eddf224f7c985c3d661341
Gegardus <stgegard@gmail.com>
2022-04-14 22:15:48 +0400

    Add interactivity

--- teacher.rb
+++ teacher.rb
@@ -3,7 +3,7 @@ require './person'
 class Teacher < Person
   def initialize(age, specialization, name = 'Unknown', parent_permission: true)
     @specialization = specialization
-    super(age, name, parent_permission)
+    super(age, name, parent_permission: parent_permission)
   end
 
   def can_use_services?
